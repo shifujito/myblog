be131f8f29c13baf55da466cd8933736c2a0ea1b
fujitoshion <43021337+FUJITOSHION@users.noreply.github.com>
2021-04-01 15:38:44 +0900

    [add] header

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -3,7 +3,13 @@ import React from 'react';
 export const Header: React.VFC = () => {
     return (
         <header>
-            <img src='/logo_transparent.png'/>
+            <img width="80px" src='/logo_transparent.png'/>
+            <nav className="header-nav">
+                <ul className="nav-ul-list">
+                    <li><a className="nav-link-style">Top</a></li>
+                    <li><a className="nav-link-style">Search</a></li>
+                </ul>
+            </nav>
         </header>
     )
 }
