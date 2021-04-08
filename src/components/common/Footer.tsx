bf269381b6541dd8cfbeadf1d3e2b5ea07357cdb
fujitoshion <43021337+FUJITOSHION@users.noreply.github.com>
2021-04-08 16:36:15 +0900

    [add] footer

---
+++ src/components/common/Footer.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const Footer: React.VFC = () => {
+    return (
+        <footer>
+            <p>© 2021 藤戸四恩 All Right Reserved.</p>
+        </footer>
+    )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,17 @@
 import ReactMarkdown from 'react-markdown';
 import { Header } from '../components/common/Header';
+import { Footer } from '../components/common/Footer';
 
 export default function Home() {
   const markdown = "# aaa\n## bbb\n### ccc";
   return (
-    <div>
+    <div >
       <Header />
-      <ReactMarkdown source={markdown} />
+      <div className="content">
+        <ReactMarkdown source={markdown} />
+        <ReactMarkdown source={markdown} />
+      </div>
+      <Footer />
     </div>
   )
 }
