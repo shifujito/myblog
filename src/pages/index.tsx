fb127e309f5077e648e48df28940db76413a788d
fujitoshion <43021337+FUJITOSHION@users.noreply.github.com>
2021-04-21 13:20:45 +0900

    add actions

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import { Header } from '../components/common/Header';
 import { Footer } from '../components/common/Footer';
 
 export default function Home() {
-  const markdown = "# aaa\n## bbb\n### ccc";
+  const markdown = "#aaa"
   return (
     <div >
       <Header />
